60134b195ee56cb1091a2569af72c33d7d9b3109
Donald Stufft <donald.stufft@gmail.com>
2012-06-21 04:14:52 -0400

    Add the packaging scripts & meta-data

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+import warehouse
+
+
+install_requires = [
+    "logan",
+]
+
+setup(
+    name="warehouse",
+    version=warehouse.__version__,
+
+    description="API Driven Python Package Index",
+    long_description=open("README.rst").read(),
+    url="https://github.com/crateio/warehouse/",
+    license=open("LICENSE").read(),
+
+    author="Donald Stufft",
+    author_email="donald.stufft@gmail.com",
+
+    install_requires=install_requires,
+
+    packages=find_packages(exclude=["tests"]),
+    zip_safe=False,
+
+    entry_points={
+        "console_scripts": [
+            "warehouse = warehouse.utils.runner:main",
+        ],
+    },
+)
