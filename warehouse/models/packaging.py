08ec2bef690d84b742bb64618dc0ce8cc685aeb5
Donald Stufft <donald.stufft@gmail.com>
2012-06-23 00:17:14 -0400

    Small grammar fix

--- warehouse/models/packaging.py
+++ warehouse/models/packaging.py
@@ -66,7 +66,7 @@ class Version(models.Model):
     platforms = dbarray.TextArrayField(blank=True)
     supported_platforms = dbarray.TextArrayField(blank=True)
 
-    # @@@ Can we convert this to a tagging system instead of a freeform text field?
+    # @@@ Can we convert this to a tagging system instead of a free form text field?
     keywords = models.TextField(blank=True)
 
     uris = hstore.DictionaryField()
