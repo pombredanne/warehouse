66b2d4f12498409291c21e991cced2c45c255f55
Donald Stufft <donald.stufft@gmail.com>
2012-07-04 17:57:35 -0400

    Make warehouse.api.resources.ModelResource handle nested resources

--- warehouse/api/resources.py
+++ warehouse/api/resources.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import include, patterns, url
 
 from tastypie.resources import ModelResource as TastypieModelResource
 from tastypie.utils import trailing_slash
@@ -10,7 +10,23 @@ __all__ = ["ModelResource"]
 class ModelResource(TastypieModelResource):
 
     def base_urls(self):
-        return [
+        urls = [
             url(r"^(?P<resource_name>%s)%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('dispatch_list'), name="api_dispatch_list"),
-            url(r"^(?P<resource_name>%s)/(?P<%s>\w[\w/-]*)%s$" % (self._meta.resource_name, self._meta.detail_uri_name, trailing_slash()), self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
+            url(r"^(?P<resource_name>%s)/(?P<%s>[^/]+)%s$" % (self._meta.resource_name, self._meta.detail_uri_name, trailing_slash()), self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
         ]
+
+        parent = getattr(self._meta, "parent_resource", None)
+        child = self
+
+        while parent is not None:
+            p = parent()
+            include_pattern = patterns("", *urls)
+
+            urls = [
+                url(r"^%s/(?P<%s__%s>[^/]+)/" % (p._meta.resource_name, child._meta.parent_resource_uri_prefix, p._meta.detail_uri_name), include(include_pattern)),
+            ]
+
+            child = parent
+            parent = getattr(p._meta, "parent_resource", None)
+
+        return urls
