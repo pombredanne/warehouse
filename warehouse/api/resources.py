3b120327afaf8b7d11d71a09108731410713d89c
Donald Stufft <donald.stufft@gmail.com>
2012-07-04 18:46:32 -0400

    Enable nested ModelResource's to link to the detail resource_uri

--- warehouse/api/resources.py
+++ warehouse/api/resources.py
@@ -1,10 +1,18 @@
 from django.conf.urls import include, patterns, url
 
+from tastypie.bundle import Bundle
 from tastypie.resources import ModelResource as TastypieModelResource
 from tastypie.utils import trailing_slash
 
 
 __all__ = ["ModelResource"]
+
+
+def _get_model_attr(obj, prefix, attr):
+    for level in prefix.split("__"):
+        obj = getattr(obj, level)
+
+    return getattr(obj, attr)
 
 
 class ModelResource(TastypieModelResource):
@@ -30,3 +38,30 @@ class ModelResource(TastypieModelResource):
             parent = getattr(p._meta, "parent_resource", None)
 
         return urls
+
+    def detail_uri_kwargs(self, bundle_or_obj):
+        if isinstance(bundle_or_obj, Bundle):
+            obj = bundle_or_obj.obj
+        else:
+            obj = bundle_or_obj
+
+        uri_kwargs = super(ModelResource, self).detail_uri_kwargs(bundle_or_obj)
+
+        parent = getattr(self._meta, "parent_resource", None)
+        child = self
+
+        prefix = ""
+
+        while parent is not None:
+            p = parent()
+
+            prefix = prefix + child._meta.parent_resource_uri_prefix
+
+            uri_kwargs.update({
+                "%s__%s" % (prefix, p._meta.detail_uri_name): _get_model_attr(obj, prefix, p._meta.detail_uri_name)
+            })
+
+            child = parent
+            parent = getattr(p._meta, "parent_resource", None)
+
+        return uri_kwargs
