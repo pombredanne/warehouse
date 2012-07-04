c38fb9727053487c80951e04024a7700a1947463
Donald Stufft <donald.stufft@gmail.com>
2012-07-04 16:28:35 -0400

    Use the normalized name for the API url instead of PK

--- warehouse/api/v1/resources/packaging.py
+++ warehouse/api/v1/resources/packaging.py
@@ -1,5 +1,8 @@
+from django.conf.urls import url
+
 from tastypie import fields
 from tastypie.resources import ModelResource
+from tastypie.utils import trailing_slash
 
 from warehouse.models import Project
 
@@ -15,9 +18,16 @@ class ProjectResource(ModelResource):
 
     class Meta:
         resource_name = "projects"
+        detail_uri_name = "normalized"
 
         queryset = Project.objects.all()
         fields = ["created", "downloads", "name", "normalized"]
 
         list_allowed_methods = ["get", "post"]
         detail_allowed_methods = ["get"]
+
+    def base_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('dispatch_list'), name="api_dispatch_list"),
+            url(r"^(?P<resource_name>%s)/(?P<%s>\w[\w/-]*)%s$" % (self._meta.resource_name, self._meta.detail_uri_name, trailing_slash()), self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
+        ]
