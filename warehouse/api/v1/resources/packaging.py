53aa5c4f28f1f4d2df3ad3ee1b99275f11793e8e
Donald Stufft <donald.stufft@gmail.com>
2012-07-04 17:58:44 -0400

    Start on a Version resource nested to a Project

--- warehouse/api/v1/resources/packaging.py
+++ warehouse/api/v1/resources/packaging.py
@@ -1,11 +1,15 @@
 from tastypie import fields
-from tastypie.constants import ALL
+from tastypie.constants import ALL, ALL_WITH_RELATIONS
 
 from warehouse.api.resources import ModelResource
-from warehouse.models import Project
+from warehouse.models import Project, Version
 
 
 # @@@ Allow deletion of projects if user has permission
+# @@@ Allow creating a new version of a project if user has permission
+# @@@ Allow editing a version of a project if user has permission
+#         - Should restrict which fields can be edited based on user
+# @@@ Can we use actual data structures for version author and maintainer
 
 
 class ProjectResource(ModelResource):
@@ -29,4 +33,33 @@ class ProjectResource(ModelResource):
         ordering = ["name", "normalized", "downloads"]
 
         list_allowed_methods = ["get", "post"]
+        detail_allowed_methods = ["get"]
+
+
+class VersionResource(ModelResource):
+
+    # Read Only Fields
+    project = fields.ToOneField(ProjectResource, "project", readonly=True)
+    version = fields.CharField(attribute="version", readonly=True)
+    # yanked
+
+    class Meta:
+        resource_name = "versions"
+        parent_resource = ProjectResource
+        parent_resource_uri_prefix = "project"
+
+        queryset = Version.objects.all()
+        fields = [
+            "created", "project", "version", "yanked",
+            "summary", "description", "license", "uris",
+            "author", "author_email", "maintainer", "maintainer_email",
+            "platforms", "supported_platforms",
+            "requires_python", "requires_external",
+        ]
+
+        filtering = {
+            "project": ALL_WITH_RELATIONS,
+        }
+
+        list_allowed_methods = ["get"]
         detail_allowed_methods = ["get"]
--- warehouse/api/v1/urls.py
+++ warehouse/api/v1/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url, include
 
-from warehouse.api.v1.resources import ProjectResource
+from warehouse.api.v1.resources import ProjectResource, VersionResource
 
 urlpatterns = patterns("",
     url(r"", include(ProjectResource().urls)),
+    url(r"", include(VersionResource().urls)),
 )
