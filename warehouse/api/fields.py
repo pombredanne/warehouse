2cd8d69c995d15c09b397c24ca93f479ac66edde
Donald Stufft <donald.stufft@gmail.com>
2012-07-17 00:21:41 -0400

    Add a field that accepts a Base64 version of a file

--- warehouse/api/fields.py
+++ warehouse/api/fields.py
@@ -1,3 +1,8 @@
+import base64
+
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+from tastypie.fields import FileField
 from tastypie.fields import ToManyField, ToOneField
 
 
@@ -22,3 +27,31 @@ class ConditionalToMany(ConditionalRelated, ToManyField):
 
 class ConditionalToOne(ConditionalRelated, ToOneField):
     pass
+
+
+class Base64FileField(FileField):
+    """
+    A django-tastypie field for handling file-uploads through raw post data.
+    It uses base64 for en-/decoding the contents of the file.
+
+    Usage:
+
+        class MyResource(ModelResource):
+            file_field = Base64FileField("file_field")
+
+            class Meta:
+                queryset = ModelWithFileField.objects.all()
+
+    In the case of multipart for submission, it would also pass the filename.
+    By using a raw post data stream, we have to pass the filename within our
+    file_field structure:
+
+        file_field = {"name": "myfile.png", "file": "longbas64encodedstring"}
+    """
+
+    def hydrate(self, obj):
+        value = super(Base64FileField, self).hydrate(obj)
+        if value:
+            value = SimpleUploadedFile(value["name"], base64.b64decode(value["file"]))
+            return value
+        return None
