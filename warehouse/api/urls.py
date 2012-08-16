e15eb6ba8806cd01cd87f2153639f7e9cd3f4de4
Donald Stufft <donald.stufft@gmail.com>
2012-08-15 23:08:49 -0400

    Added a skeleton for the simple API

--- warehouse/api/urls.py
+++ warehouse/api/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns("",
     url(r"^v1/", include("warehouse.api.v1.urls")),
+    url(r"^simple/", include("warehouse.api.simple.urls")),
 )
