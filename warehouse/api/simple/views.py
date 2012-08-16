c70146f9ff748f7fb1a745bae50d9121965641ea
Donald Stufft <donald.stufft@gmail.com>
2012-08-15 23:35:16 -0400

    Remove unneeded code

--- warehouse/api/simple/views.py
+++ warehouse/api/simple/views.py
@@ -8,7 +8,3 @@ class ProjectIndex(ListView):
     restricted = False
     queryset = Project.objects.all().order_by("name")
     template_name = "api/simple/index.html"
-
-    #@method_decorator(cache_page(60 * 15))
-    def dispatch(self, *args, **kwargs):
-        return super(ProjectIndex, self).dispatch(*args, **kwargs)
