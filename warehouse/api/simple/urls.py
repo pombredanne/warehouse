7ac2823f5f58d13ab4a8620b69b55b7edd6f448e
Donald Stufft <donald.stufft@gmail.com>
2012-08-15 23:45:26 -0400

    Name the simple index

--- warehouse/api/simple/urls.py
+++ warehouse/api/simple/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 from warehouse.api.simple.views import ProjectIndex
 
 urlpatterns = patterns("",
-    url(r"^$", ProjectIndex.as_view())
+    url(r"^$", ProjectIndex.as_view(), name="api_simple_index")
 )
