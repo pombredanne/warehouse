4c51efbcb1fe79f8b00e691e21baae0f5ba310e5
Donald Stufft <donald.stufft@gmail.com>
2012-08-17 00:59:46 -0400

    Allow filtering by date

--- warehouse/api/v1/resources/downloads.py
+++ warehouse/api/v1/resources/downloads.py
@@ -24,7 +24,7 @@ class DownloadResource(ModelResource):
             "project": ALL,
             "version": ALL,
             "filename": ALL,
-            "datetime": ALL,
+            "date": ALL,
         }
 
         authentication = MultiAuthentication(BasicAuthentication())
