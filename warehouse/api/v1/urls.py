6126e22504c9a1f94d66e1fc35a5af6496318f60
Donald Stufft <donald.stufft@gmail.com>
2012-08-17 02:48:51 -0400

    Switch to using an Api class now that nested is gone

--- warehouse/api/v1/urls.py
+++ warehouse/api/v1/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import patterns, url, include
 
+from tastypie.api import Api
+
 from warehouse.api.v1.resources import ProjectResource, VersionResource, FileResource, DownloadResource
 
+v1_api = Api(api_name="v1")
+v1_api.register(ProjectResource())
+v1_api.register(VersionResource())
+v1_api.register(FileResource())
+v1_api.register(DownloadResource())
+
 urlpatterns = patterns("",
-    url(r"", include(ProjectResource().urls)),
-    url(r"", include(VersionResource().urls)),
-    url(r"", include(FileResource().urls)),
-    url(r"", include(DownloadResource().urls)),
+    url(r"", include(v1_api.urls)),
 )
