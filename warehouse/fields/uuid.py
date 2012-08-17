ce2fcbbf8a6dbf812809b7e99a98e6fce21efa2e
Donald Stufft <donald.stufft@gmail.com>
2012-08-16 21:44:08 -0400

    Add UUIDField

---
+++ warehouse/fields/uuid.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+
+import psycopg2.extras
+import uuid
+
+from django.db import models
+
+# Register the adapter so we can use UUID objects.
+psycopg2.extras.register_uuid()
+
+
+class UUIDField(models.CharField):
+    __metaclass__ = models.SubfieldBase
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("default", uuid.uuid4)
+        kwargs.setdefault("editable", not kwargs.get("primary_key", False))
+
+        super(UUIDField, self).__init__(*args, **kwargs)
+
+    def db_type(self):
+        return "uuid"
+
+    def get_db_prep_value(self, value):
+        return self.to_python(value)
+
+    def to_python(self, value):
+        if not value:
+            return None
+        if not isinstance(value, uuid.UUID):
+            value = uuid.UUID(value)
+        return value
