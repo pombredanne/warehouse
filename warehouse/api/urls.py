3fddda16172be2b3b36fa79ac9090a527ffa5ed1
Donald Stufft <donald.stufft@gmail.com>
2012-08-17 02:53:50 -0400

    Move around defintions to clean up things a bit

--- warehouse/api/urls.py
+++ warehouse/api/urls.py
@@ -1,7 +1,17 @@
 from django.conf.urls import patterns, include, url
 
+from tastypie.api import Api
+
+import warehouse.api.v1.resources
+
+v1_api = Api(api_name="v1")
+v1_api.register(warehouse.api.v1.resources.ProjectResource())
+v1_api.register(warehouse.api.v1.resources.VersionResource())
+v1_api.register(warehouse.api.v1.resources.FileResource())
+v1_api.register(warehouse.api.v1.resources.DownloadResource())
+
 urlpatterns = patterns("",
-    url(r"^v1/", include("warehouse.api.v1.urls")),
     url(r"^simple/", include("warehouse.api.simple.urls")),
     url(r"^restricted/", include("warehouse.api.simple.restricted")),
+    url(r"", include(v1_api.urls)),
 )
