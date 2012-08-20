0bcb8805e74c69938dc63affa8ef105e10a2cf9f
Donald Stufft <donald.stufft@gmail.com>
2012-08-20 16:42:47 -0400

    Add a command to spawn an RQ worker

---
+++ warehouse/management/commands/worker.py
@@ -0,0 +1,63 @@
+from optparse import make_option
+
+import redis
+import rq
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    args = "<queue queue ...>"
+    help = "Starts an RQ worker"
+
+    option_list = BaseCommand.option_list + (
+        make_option("--host", "-H",
+            action="store",
+            dest="host",
+            help="The Redis hostname (default: localhost)"
+        ),
+        make_option("--port", "-p",
+            action="store",
+            dest="port",
+            help="The Redis port number (default: 6379)"
+        ),
+        make_option("--db", "-d",
+            action="store",
+            dest="db",
+            help="The Redis database (default: 0)"
+        ),
+        make_option("--password", "-P",
+            action="store",
+            dest="password",
+            help="The password for Redis"
+        ),
+        make_option("--burst", "-b",
+            action="store_true",
+            dest="burst",
+            default=False,
+            help="Run in burst mode (quit after all work is done)"
+        ),
+        make_option("--name", "-n",
+            action="store",
+            dest="name",
+            help="Specify a different name",
+        ),
+    )
+
+    def handle(self, *args, **options):
+        if not args:
+            args = ["default"]
+
+        host = options.get("host", None) or getattr(settings, "RQ_REDIS_HOST", "localhost")
+        port = options.get("port", None) or getattr(settings, "RQ_REDIS_PORT", 6379)
+        password = options.get("password", None) or getattr(settings, "RQ_REDIS_PASSWORD", None)
+        db = options.get("db", None) or getattr(settings, "RQ_REDIS_DB", 0)
+
+        print host, port, password, db
+
+        conn = redis.Redis(host=host, port=port, db=db, password=password)
+
+        with rq.Connection(conn):
+            worker = rq.Worker([rq.Queue(q) for q in args], name=options.get("name", getattr(settings, "RQ_NAME", None)))
+            worker.work(burst=options.get("burst"))
