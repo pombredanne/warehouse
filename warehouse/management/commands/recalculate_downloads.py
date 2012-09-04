b7fab3e9a2de7ccf6064cd2dbb92f601503aa343
Donald Stufft <donald.stufft@gmail.com>
2012-09-03 20:11:35 -0400

    simplify the recalculation and fix a bug where downloads might not get set to 0

--- warehouse/management/commands/recalculate_downloads.py
+++ warehouse/management/commands/recalculate_downloads.py
@@ -23,38 +23,24 @@ class Command(NoArgsCommand):
         cursor = connection.cursor()
         downloads = connection.connection.cursor(name=str(uuid.uuid4()))
 
-        seen = set()
-
         total = 0
 
         with transaction.commit_manually():
             try:
+                # Set all the download counts to 0
+                cursor.execute("UPDATE warehouse_project SET downloads = 0")
+                cursor.execute("UPDATE warehouse_version SET downloads = 0")
+                cursor.execute("UPDATE warehouse_versionfile SET downloads = 0")
+
+                # Replay all the download counts
                 downloads.execute("SELECT project, version, filename, downloads FROM warehouse_download")
 
                 for i, record in enumerate(downloads):
-                    # Check if Project has not been seen and set downloads to 0 then
-                    pids, vids = None, None
-
-                    if not record[0] in seen:
-                        logger.debug("Resetting download counts for %s", record[0])
-                        cursor.execute("UPDATE warehouse_project SET downloads = 0 WHERE name = %s RETURNING id", [record[0]])
-                        pids = cursor.fetchall()
-
-                        if pids:
-                            cursor.execute("UPDATE warehouse_version SET downloads = 0 WHERE project_id IN %s RETURNING id", [tuple([r[0] for r in pids])])
-                            vids = cursor.fetchall()
-
-                        if vids:
-                            cursor.execute("UPDATE warehouse_versionfile SET downloads = 0 WHERE version_id IN %s", [tuple([r[0] for r in vids])])
-
                     # Update download counts
                     Download.update_counts(record[0], record[1], record[2], record[3])
 
                     # Update running total
                     total += record[3]
-
-                    # We've now seen this Project
-                    seen.add(record[0])
             except:
                 transaction.rollback()
                 raise
