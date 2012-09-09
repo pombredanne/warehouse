eedee3d9ab87447e90e8da99c004022e4d215e82
Donald Stufft <donald.stufft@gmail.com>
2012-09-09 13:22:17 -0400

    Move the imports after the environ setdefault's

--- warehouse/__main__.py
+++ warehouse/__main__.py
@@ -2,12 +2,12 @@
 import os
 import sys
 
-from configurations.management import execute_from_command_line
-
 
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "warehouse.settings.base")
     os.environ.setdefault("DJANGO_CONFIGURATION", "WarehouseSettings")
+
+    from configurations.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
 
--- warehouse/wsgi.py
+++ warehouse/wsgi.py
@@ -1,8 +1,8 @@
 import os
 
-from configurations.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "warehouse.settings.base")
 os.environ.setdefault("DJANGO_CONFIGURATION", "WarehouseSettings")
+
+from configurations.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
