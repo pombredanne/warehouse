81c2a4e1cc5568fb5a039763c72f65806780673c
Donald Stufft <donald.stufft@gmail.com>
2012-09-09 13:14:11 -0400

    Migrate base config to django-configuration

--- warehouse/settings/base.py
+++ warehouse/settings/base.py
@@ -1,97 +1,127 @@
-from django.conf.global_settings import *
-
 import os
 import os.path
 
-# Constants
-PROJECT_ROOT = os.path.join(os.path.dirname(__file__), os.pardir)
+from configurations import Settings
 
 
-# Settings
-DEBUG = False
-TEMPLATE_DEBUG = True
+class WarehouseSettings(Settings):
+    PROJECT_ROOT = os.path.join(os.path.dirname(__file__), os.pardir)
 
-ADMINS = []
+    DEBUG = False
+    TEMPLATE_DEBUG = True
 
-MANAGERS = ADMINS
+    ADMINS = []
+    MANAGERS = ADMINS
 
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
+    INTERNAL_IPS = [
+        "127.0.0.1",
+    ]
 
-APPEND_SLASH = True
+    APPEND_SLASH = True
 
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.dummy.DummyCache",
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.dummy.DummyCache",
+        }
     }
-}
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = "UTC"
+    SITE_ID = 1
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = "en"
+    ROOT_URLCONF = "warehouse.urls"
 
-SITE_ID = 1
+    # Local time zone for this installation. Choices can be found here:
+    # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+    # although not all choices may be available on all operating systems.
+    # On Unix systems, a value of None will cause Django to use the same
+    # timezone as the operating system.
+    # If running in a Windows environment this must be set to the same as your
+    # system time zone.
+    TIME_ZONE = "UTC"
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = False
+    # Language code for this installation. All choices can be found here:
+    # http://www.i18nguy.com/unicode/language-identifiers.html
+    LANGUAGE_CODE = "en"
 
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
-USE_L10N = False
+    # If you set this to False, Django will make some optimizations so as not
+    # to load the internationalization machinery.
+    USE_I18N = False
 
-USE_TZ = True
+    # If you set this to False, Django will not format dates, numbers and
+    # calendars according to the current locale
+    USE_L10N = False
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    "django.template.loaders.filesystem.Loader",
-    "django.template.loaders.app_directories.Loader",
-)
+    # If you set this to False, Django will not use timezone-aware datetimes.
+    USE_TZ = True
 
-MIDDLEWARE_CLASSES = (
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.http.ConditionalGetMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-)
+    MIDDLEWARE_CLASSES = [
+        "django.middleware.gzip.GZipMiddleware",
+        "django.middleware.common.CommonMiddleware",
+        "django.middleware.http.ConditionalGetMiddleware",
+        "django.contrib.sessions.middleware.SessionMiddleware",
+        "django.middleware.csrf.CsrfViewMiddleware",
+        "django.contrib.auth.middleware.AuthenticationMiddleware",
+        "django.contrib.messages.middleware.MessageMiddleware",
+    ]
 
-ROOT_URLCONF = "warehouse.urls"
+    # List of callables that know how to import templates from various sources.
+    TEMPLATE_LOADERS = [
+        "django.template.loaders.filesystem.Loader",
+        "django.template.loaders.app_directories.Loader",
+    ]
 
-TEMPLATE_DIRS = [
-    os.path.join(PROJECT_ROOT, "templates"),
-]
+    TEMPLATE_DIRS = [
+        os.path.join(PROJECT_ROOT, "templates"),
+    ]
 
-INSTALLED_APPS = (
-    "django.contrib.auth",
-    "django.contrib.admin",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.sites",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+    INSTALLED_APPS = [
+        "django.contrib.auth",
+        "django.contrib.admin",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.sites",
+        "django.contrib.messages",
+        "django.contrib.staticfiles",
 
-    # External
-    "haystack",
-    "json_field",
-    "south",
-    "tastypie",
+        # External
+        "haystack",
+        "json_field",
+        "south",
+        "tastypie",
 
-    # Internal
-    "warehouse",
-)
+        # Internal
+        "warehouse",
+    ]
 
-EMAIL_SUBJECT_PREFIX = "[Warehouse] "
+    EMAIL_SUBJECT_PREFIX = "[Warehouse] "
 
-STATIC_URL = "/site_media/static/"
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+
+        "formatters": {
+            "console": {
+                "format": "[%(asctime)s] [%(levelname)s] [%(name)s]  %(message)s",
+                "datefmt": "%H:%M:%S",
+            },
+        },
+
+        "handlers": {
+            "console": {
+                "level": "DEBUG",
+                "class": "logging.StreamHandler",
+                "formatter": "console",
+            },
+        },
+
+        "root": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+
+        "loggers": {
+            "requests.packages.urllib3": {
+                "handlers": [],
+                "propagate": False,
+            },
+        },
+    }
