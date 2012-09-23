4dea404b35546420f2a4bca41796844756993d93
Donald Stufft <donald.stufft@gmail.com>
2012-09-22 23:24:36 -0400

    Switch to using xact and shorten the transaction block a bit

--- warehouse/management/commands/recalculate_downloads.py
+++ warehouse/management/commands/recalculate_downloads.py
@@ -6,10 +6,11 @@ import progress.bar
 import redis
 
 from django.core.management.base import NoArgsCommand
-from django.db import connection, transaction
+from django.db import connection
 
 from warehouse.conf import settings
 from warehouse.models import Download
+from warehouse.utils.transactions import xact
 
 
 logger = logging.getLogger(__name__)
@@ -27,33 +28,27 @@ class Command(NoArgsCommand):
 
         totals = collections.Counter()
 
-        with transaction.commit_manually():
-            try:
-                # Set all the download counts to 0
-                cursor.execute("UPDATE warehouse_project SET downloads = 0")
-                cursor.execute("UPDATE warehouse_version SET downloads = 0")
-                cursor.execute("UPDATE warehouse_versionfile SET downloads = 0")
+        # Get the number of downloads
+        cursor.execute("SELECT reltuples FROM pg_class WHERE relname=%s", ["warehouse_download"])
+        count = int(cursor.fetchall()[0][0])
 
-                # Get the number of downloads
-                cursor.execute("SELECT reltuples FROM pg_class WHERE relname=%s", ["warehouse_download"])
-                count = int(cursor.fetchall()[0][0])
+        logger.info("Found %s download records", count)
 
-                logger.info("Found %s download records", count)
+        # Replay all the download counts
+        downloads.execute("SELECT project, filename, downloads FROM warehouse_download")
 
-                # Replay all the download counts
-                downloads.execute("SELECT project, filename, downloads FROM warehouse_download")
+        for record in progress.bar.ShadyBar("Recalculating", max=count).iter(downloads):
+            # Process Record
+            totals[(record[0], record[1])] += record[2]
 
-                for record in progress.bar.ShadyBar("Recalculating", max=count).iter(downloads):
-                    # Process Record
-                    totals[(record[0], record[1])] += record[2]
+        with xact():
+            # Set all the download counts to 0
+            cursor.execute("UPDATE warehouse_project SET downloads = 0")
+            cursor.execute("UPDATE warehouse_version SET downloads = 0")
+            cursor.execute("UPDATE warehouse_versionfile SET downloads = 0")
 
-                for (project, filename), changed in progress.bar.ShadyBar("Updating Counts", max=len(totals)).iter(totals.iteritems()):
-                    Download.update_counts(project, filename, changed)
-            except:
-                transaction.rollback()
-                raise
-            else:
-                transaction.commit()
+            for (project, filename), changed in progress.bar.ShadyBar("Updating Counts", max=len(totals)).iter(totals.iteritems()):
+                Download.update_counts(project, filename, changed)
 
         datastore = redis.StrictRedis(**dict([(k.lower(), v) for k, v in settings.REDIS.items()]))
         datastore.set("warehouse:stats:downloads", sum(totals.values()))
