383dd84904d6f0d55cc3c721a908e011d23e7277
Donald Stufft <donald.stufft@gmail.com>
2012-09-26 01:34:32 -0400

    Remove explicitly adding "" to the sys.path

--- warehouse/__main__.py
+++ warehouse/__main__.py
@@ -3,11 +3,7 @@ import sys
 
 
 def main():
-    # @@@ Why is this required?
-    sys.path = [""] + sys.path
-
     from configurations.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
 
 
