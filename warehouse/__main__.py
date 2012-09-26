754f2f4fa9e246792db71d400adbdcf832145197
Donald Stufft <donald.stufft@gmail.com>
2012-09-26 01:33:37 -0400

    Remove the default settings as it's no longer an option

--- warehouse/__main__.py
+++ warehouse/__main__.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
-import os
 import sys
 
 
 def main():
     # @@@ Why is this required?
     sys.path = [""] + sys.path
-
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "warehouse.settings.base")
-    os.environ.setdefault("DJANGO_CONFIGURATION", "WarehouseSettings")
 
     from configurations.management import execute_from_command_line
 
