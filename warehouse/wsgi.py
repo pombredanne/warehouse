73c3c72b89426bface57a5ed75a6a7aeda3aa2b7
Donald Stufft <donald.stufft@gmail.com>
2012-09-26 01:40:23 -0400

    Remove default configuration values from warehouse.wsgi

--- warehouse/wsgi.py
+++ warehouse/wsgi.py
@@ -1,8 +1,3 @@
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "warehouse.settings.base")
-os.environ.setdefault("DJANGO_CONFIGURATION", "WarehouseSettings")
-
 from configurations.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
