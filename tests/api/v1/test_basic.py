0f20785293248a771f2c9e2d8371bdc503566239
Donald Stufft <donald.stufft@gmail.com>
2012-09-27 10:48:43 -0400

    Test the Top level api as well

--- tests/api/v1/test_basic.py
+++ tests/api/v1/test_basic.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-@pytest.mark.parametrize("url", ["/v1/projects/", "/v1/versions/", "/v1/files/"])
+@pytest.mark.parametrize("url", ["/v1/", "/v1/projects/", "/v1/versions/", "/v1/files/"])
 def test_unauthenticated_access(client, url):
     resp = client.get(url, ACCEPT="application/json")
     assert resp.status_code == 200
