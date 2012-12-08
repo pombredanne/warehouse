c5d0595acb080bdc33efdc95a5781ed6b87b0a2e
Donald Stufft <donald.stufft@gmail.com>
2012-12-07 21:52:47 -0500

    Refactor Project to use new mixins and methods

--- warehouse/packages/models.py
+++ warehouse/packages/models.py
@@ -2,18 +2,37 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import unicode_literals
 
-from sqlalchemy import event
 from sqlalchemy.schema import FetchedValue
 from sqlalchemy.dialects import postgresql as pg
+from sqlalchemy.ext.declarative import declared_attr
 
 from warehouse import db
-from warehouse.databases.mixins import UUIDPrimaryKeyMixin
+from warehouse.database.mixins import UUIDPrimaryKeyMixin, TimeStampedMixin
+from warehouse.database.schema import TableDDL
 from warehouse.database.types import CIText
+from warehouse.database.utils import table_args
 
 
-class Project(UUIDPrimaryKeyMixin, db.Model):
+class Project(UUIDPrimaryKeyMixin, TimeStampedMixin, db.Model):
 
     __tablename__ = "projects"
+    __table_args__ = declared_attr(table_args((
+            TableDDL("""
+                    CREATE OR REPLACE FUNCTION normalize_name()
+                    RETURNS trigger AS $$
+                    BEGIN
+                        NEW.normalized = lower(regexp_replace(new.name, '[^A-Za-z0-9.]+', '-'));
+                        return NEW;
+                    END;
+                    $$ LANGUAGE plpgsql;
+
+                    CREATE TRIGGER %(table)s_normalize_name
+                    BEFORE INSERT OR UPDATE
+                    ON %(table)s
+                    FOR EACH ROW
+                    EXECUTE PROCEDURE normalize_name();
+                """),
+        )))
 
     name = db.Column(CIText, unique=True, nullable=False)
     normalized = db.Column(CIText, unique=True, nullable=False,
@@ -25,21 +44,3 @@ class Project(UUIDPrimaryKeyMixin, db.Model):
 
     def __repr__(self):
         return "<Project: {name}>".format(name=self.name)
-
-
-# Create the Trigger to fill in normalized (TODO: Move this to Alembic?)
-event.listen(Project.__table__, "after_create", db.DDL("""
-CREATE OR REPLACE FUNCTION normalize_name() RETURNS trigger AS
-$body$
-BEGIN
-    new.normalized = lower(regexp_replace(new.name, '[^A-Za-z0-9.]+', '-'));
-    RETURN new;
-end;
-$body$ LANGUAGE plpgsql;
-
-CREATE TRIGGER %(table)s_normalize_name
-BEFORE INSERT OR UPDATE
-ON %(table)s
-FOR EACH ROW
-EXECUTE PROCEDURE normalize_name();
-"""))
