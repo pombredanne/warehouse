b307bb4e59598670cf2ee09c4107d9f42d8b7d3c
Donald Stufft <donald.stufft@gmail.com>
2012-12-07 21:50:27 -0500

    Add a database mixin for Timestamping models

--- warehouse/database/mixins.py
+++ warehouse/database/mixins.py
@@ -1,10 +1,36 @@
 from sqlalchemy.dialects import postgresql as pg
+from sqlalchemy.sql import func
 from sqlalchemy.sql.expression import text
 
 from warehouse import db
+from warehouse.database.schema import TableDDL
 
 
 class UUIDPrimaryKeyMixin(object):
 
     id = db.Column(pg.UUID(as_uuid=True),
             primary_key=True, server_default=text("uuid_generate_v4()"))
+
+
+class TimeStampedMixin(object):
+
+    __table_args__ = (
+        TableDDL("""
+                CREATE OR REPLACE FUNCTION update_modified_column()
+                RETURNS TRIGGER AS $$
+                BEGIN
+                    NEW.modified = now();
+                    RETURN NEW;
+                END;
+                $$ LANGUAGE 'plpgsql';
+
+                CREATE TRIGGER update_%(table)s_modtime
+                BEFORE UPDATE
+                ON %(table)s
+                FOR EACH ROW
+                EXECUTE PROCEDURE update_modified_column();
+            """),
+    )
+
+    created = db.Column(db.DateTime, server_default=func.now(), nullable=False)
+    modified = db.Column(db.DateTime, server_default=func.now(), nullable=False)
