a4368610c2205327f84e63aff83364ec175817a7
Donald Stufft <donald.stufft@gmail.com>
2012-12-13 19:58:55 -0500

    Refactor the synchonrize command
    * Moves database access into it's own module (``warehouse.packages.store``).
    * Uses eventlet to enable async IO
    * Uses progress to present a useful progress bar to the user
    * Has hackish support for saving files to the filesystem

---
+++ warehouse/packages/store.py
@@ -0,0 +1,109 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+
+import hashlib
+import os
+
+import flask
+
+from sqlalchemy.orm.exc import NoResultFound
+
+from warehouse import db
+from warehouse.packages.models import Project, Version, File, FileType
+
+
+def project(name):
+    try:
+        project = Project.query.filter_by(name=name).one()
+    except NoResultFound:
+        project = Project(name)
+        db.session.add(project)
+
+    return project
+
+
+def version(project, release):
+    try:
+        version = Version.query.filter_by(project=project,
+                                          version=release["version"]).one()
+    except NoResultFound:
+        version = Version(project=project, version=release["version"])
+
+    version.summary = release.get("summary", "")
+    version.description = release.get("description", "")
+
+    version.author = release.get("author", "")
+    version.author_email = release.get("author_email", "")
+
+    version.maintainer = release.get("maintainer", "")
+    version.maintainer_email = release.get("maintainer_email", "")
+
+    version.license = release.get("license", "")
+
+    version.requires_python = release.get("requires_python", "")
+    version.requires_external = release.get("requires_external", [])
+
+    version.keywords = release.get("keywords", [])
+
+    version.uris = release.get("uris", {})
+
+    version.download_uri = release.get("download_uri", "")
+
+    db.session.add(version)
+
+    return version
+
+
+def distribution(project, version, dist):
+    try:
+        vfile = File.query.filter_by(version=version,
+                                     filename=dist["filename"]).one()
+    except NoResultFound:
+        vfile = File(version=version, filename=dist["filename"])
+
+        vfile.filesize = dist["filesize"]
+        vfile.python_version = dist["python_version"]
+
+        vfile.type = FileType.from_string(dist["type"])
+
+        vfile.comment = dist.get("comment", "")
+
+    db.session.add(vfile)
+
+    return vfile
+
+
+def distribution_file(project, version, distribution, dist_file):
+    def _prefix(project, version, distribution, dist_file):
+        # Create a Hash based URL prefix
+        # TODO(dstufft): Determine which algo to use
+        digest = hashlib.md5(dist_file).hexdigest()
+        parts = list(digest[:5])
+        parts.append(digest)
+        return os.path.join(*parts)
+
+    # TODO(dstufft): Don't hard code this, and make it generic file storage
+    directory = flask.current_app.config["WAREHOUSE_STORAGE_DIRECTORY"]
+
+    filename = os.path.join(
+                directory,
+                _prefix(project, version, distribution, dist_file),
+                distribution.filename
+            )
+
+    try:
+        os.makedirs(os.path.dirname(filename))
+    except OSError:
+        pass
+
+    with open(filename, "w") as fp:
+        fp.write(dist_file)
+
+    hashes = {}
+
+    for algorithm in hashlib.algorithms:
+        hashes[algorithm] = getattr(hashlib, algorithm)(dist_file).hexdigest()
+
+    distribution.hashes = hashes
+    distribution.file = filename
--- warehouse/synchronize/commands.py
+++ warehouse/synchronize/commands.py
@@ -3,11 +3,15 @@ from __future__ import division
 from __future__ import unicode_literals
 
 import eventlet
+import flask
+import requests
 import xmlrpc2.client
 
+from progress.bar import ShadyBar
 from sqlalchemy.orm.exc import NoResultFound
 
-from warehouse import db, script
+from warehouse import create_app, db, script
+from warehouse.packages import store
 from warehouse.packages.models import Project, Version, FileType, File
 from warehouse.synchronize import validators
 
@@ -38,27 +42,43 @@ class PyPIFetcher(object):
         # TODO(dstufft): Switch this to using verified SSL
         self.client = xmlrpc2.client.Client("http://pypi.python.org/pypi")
 
-    def files(self, project, version):
+    def file(self, url):
+        """
+        Fetches the file located at ``url``.
+        """
+        resp = requests.get(url, prefetch=True)
+        return resp.content
+
+    def distributions(self, project, version):
         """
         Takes a project and version and it returns the normalized files for
         the release of project with the given version.
         """
-        files = self.client.release_urls(project, version)
-        files = [filter_dict(x) for x in files]
+        urls = self.client.release_urls(project, version)
 
         # TODO(dstufft): Validate incoming data
 
-        # Filter resulting dictionaries down to only the required keys
         keys = {
-            "filename", "md5_digest", "size", "python_version", "packagetype",
-            "comment_text",
+            "filename", "filesize", "python_version", "type", "comment",
+            "md5_digest", "url",
         }
-        files = [{key: value for key, value in x.items() if key in keys}
-                    for x in files]
 
-        return files
+        for url in urls:
+            url = filter_dict(url)
 
-    def version(self, project, version):
+            # Rename size to filesize
+            url["filesize"] = url["size"]
+
+            # Rename packagetype to type
+            url["type"] = url["packagetype"]
+
+            # Rename comment_text to comment
+            if "comment_text" in url:
+                url["comment"] = url["comment_text"]
+
+            yield {key: value for key, value in url.items() if key in keys}
+
+    def release(self, project, version):
         """
         Takes a project and version and it returns the normalized data for the
         release of project with that version.
@@ -71,140 +91,85 @@ class PyPIFetcher(object):
         data["classifiers"] = list(set(data.get("classifiers", [])))
         data["classifiers"].sort()
 
+        # rename download_url to download_uri
+        if "download_url" in data:
+            data["download_uri"] = data["download_url"]
+
+        # Collapse project_url, bugtrack_url, and home_page into uris
+        data["uris"] = {}
+
+        if "project_url" in data:
+            data["uris"].update(data["project_url"])
+
+        if "bugtrack_url" in data:
+            data["uris"]["bugtracker"] = data["bugtrack_url"]
+
+        if "home_page" in data:
+            data["uris"]["home page"] = data["home_page"]
+
         # Filter resulting dictionary down to only the required keys
         keys = {
             "name", "version", "summary", "description", "author",
             "author_email", "maintainer", "maintainer_email", "license",
-            "requires_python", "requires_external", "bugtrack_url",
-            "home_page", "project_url", "keywords", "download_url",
+            "requires_python", "requires_external", "uris", "keywords",
+            "download_uri",
         }
-        data = {key: value for key, value in data.items() if key in keys}
 
-        data["files"] = self.files(project, version)
+        return {key: value for key, value in data.items() if key in keys}
 
-        return data
-
-    def project(self, project):
+    def versions(self, project):
         """
-        Takes a project and returns all the normalized data for all the
-        versions of that project.
+        Returns a list of all the versions for a particular project.
         """
         versions = self.client.package_releases(project, True)
-        versions = validators.package_releases.validate(versions)
+        return validators.package_releases.validate(versions)
 
-        return [self.version(project, v) for v in versions]
-
-    def packages(self):
+    def projects(self):
         """
         Returns a list of all project names
         """
         return validators.list_packages.validate(self.client.list_packages())
 
 
-def store_project(name):
-    try:
-        project = Project.query.filter_by(name=name).one()
-    except NoResultFound:
-        project = Project(name)
-        db.session.add(project)
-        db.session.flush()
+def synchronize_project(app, project, fetcher):
+    with app.test_request_context():
+        project = store.project(project)
 
-    return project
+        for v in fetcher.versions(project.name):
+            version = store.version(project, fetcher.release(project.name, v))
 
+            for dist in fetcher.distributions(project.name, version.version):
+                distribution = store.distribution(project, version, dist)
 
-def store(release):
-    project = store_project(release["name"])
+                # Check if the stored hash matches what the fetcher says
+                if (distribution.hashes is None or
+                        dist["md5_digest"] != distribution.hashes.get("md5")):
+                    # The fetcher has a different file
+                    # TODO(dstufft): Verify that this url is HTTPS
+                    store.distribution_file(project, version, distribution,
+                                            fetcher.file(dist["url"]))
 
-    try:
-        version = Version.query.filter_by(project=project,
-                                          version=release["version"]).one()
-    except NoResultFound:
-        version = Version(project=project, version=release["version"])
-        db.session.add(version)
-        db.session.flush()
-
-    version.summary = release.get("summary", "")
-    version.description = release.get("description", "")
-
-    version.author = release.get("author", "")
-    version.author_email = release.get("author_email", "")
-
-    version.maintainer = release.get("maintainer", "")
-    version.maintainer_email = release.get("maintainer_email", "")
-
-    version.license = release.get("maintainer", "")
-
-    version.requires_python = release.get("requires_python", "")
-    version.requires_external = release.get("requires_external", [])
-
-    version.keywords = release.get("keywords", [])
-
-    version.uris = release.get("project_url", {})
-
-    if release.get("bugtrack_url"):
-        version.uris.update({"Bugtracker": release["bugtrack_url"]})
-
-    if release.get("home_page"):
-        version.uris.update({"Home page": release["home_page"]})
-
-    version.download_uri = release.get("download_url", "")
-
-    # TODO(dstufft): Remove no longer existing Files
-
-    for release_file in release.get("files", []):
-        try:
-            vfile = File.query.filter_by(version=version,
-                                         filename=release_file["filename"]
-                                         ).one()
-        except NoResultFound:
-            vfile = File(version=version, filename=release_file["filename"])
-            db.session.add(vfile)
-
-        vfile.filesize = release_file["size"]
-        vfile.python_version = release_file["python_version"]
-
-        vfile.type = FileType.from_string(release_file["packagetype"])
-
-        vfile.comment = release_file.get("comment_text", "")
-
-        db.session.flush()
-
-    return project, version
+        # Commit our changes
+        db.session.commit()
 
 
-def syncer(project=None, version=None, fetcher=None):
-    if project is None and not version is None:
-        raise TypeError("Cannot have a version without a project")
+def syncer(projects=None, fetcher=None, app=None, pool=None):
+    if pool is None:
+        pool = eventlet.GreenPool(10)
+
+    if app is None:
+        app = create_app()
 
     if fetcher is None:
         fetcher = PyPIFetcher()
 
-    projects = [project] if not project is None else fetcher.packages()
+    if projects is None:
+        # TODO(dstufft): Determine how to make this do the "since last sync"
+        projects = fetcher.projects()
 
-    if version is None:
-        pool = eventlet.GreenPool(100)
-
-        for releases in pool.imap(fetcher.project, projects):
-            # Take the releases and add them to the database
-            for release in releases:
-                store(release)
-
-            # TODO(dstufft): Remove no longer existing releases
-
-            # Commit the session to the Database
-            db.session.commit()
-
-        # Make sure that all projects exist in the database
-        for project in projects:
-            store_project(project)
-    else:
-        # Fetch and store the release
-        store(fetcher.version(project, version))
-
-    # TODO(dstufft): Remove no longer existing projects
-
-    # Commit the session to the Database
-    db.session.commit()
+    with app.app_context():
+        for project in ShadyBar("Syncing", max=len(projects)).iter(projects):
+            pool.spawn_n(synchronize_project, app, project, fetcher)
 
 
 @script.command
