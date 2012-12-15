0640056e85e9a7ead2e05ac9934dc2ec47bdea18
Donald Stufft <donald.stufft@gmail.com>
2012-12-14 21:13:40 -0500

    Add classifiers to the data models

--- warehouse/packages/models.py
+++ warehouse/packages/models.py
@@ -4,9 +4,11 @@ from __future__ import unicode_literals
 
 from sqlalchemy.schema import FetchedValue
 from sqlalchemy.dialects import postgresql as pg
+from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.ext.mutable import MutableDict
 from sqlalchemy.orm import relationship
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.sql.expression import text
 
 from warehouse import db
@@ -14,6 +16,41 @@ from warehouse.database.mixins import UUIDPrimaryKeyMixin, TimeStampedMixin
 from warehouse.database.schema import TableDDL
 from warehouse.database.types import Enum
 from warehouse.database.utils import table_args
+
+
+classifiers = db.Table("version_classifiers",
+    db.Column("id", pg.UUID(as_uuid=True), primary_key=True,
+              server_default=text("uuid_generate_v4()")),
+    db.Column("classifier_id", pg.UUID(as_uuid=True),
+              db.ForeignKey("classifiers.id", ondelete="CASCADE"),
+              nullable=False),
+    db.Column("version_id", pg.UUID(as_uuid=True),
+              db.ForeignKey("versions.id", ondelete="CASCADE"),
+              nullable=False),
+    db.Index("idx_version_classifiers", "classifier_id", "version_id",
+             unique=True),
+)
+
+
+class Classifier(UUIDPrimaryKeyMixin, db.Model):
+
+    __tablename__ = "classifiers"
+
+    trove = db.Column(db.UnicodeText, unique=True, nullable=False)
+
+    def __init__(self, trove):
+        self.trove = trove
+
+    def __repr__(self):
+        return "<Classifier: {trove}>".format(trove=self.trove)
+
+    @classmethod
+    def get_or_create(cls, trove):
+        try:
+            obj = cls.query.filter_by(trove=trove).one()
+        except NoResultFound:
+            obj = cls(trove)
+        return obj
 
 
 class Project(UUIDPrimaryKeyMixin, TimeStampedMixin, db.Model):
@@ -89,7 +126,11 @@ class Version(UUIDPrimaryKeyMixin, TimeStampedMixin, db.Model):
     requires_external = db.Column(pg.ARRAY(db.UnicodeText, dimensions=1),
                                   nullable=False, server_default="{}")
 
-    files = relationship("File", backref="version")
+    _classifiers = relationship("Classifier", secondary=classifiers,
+                               backref=db.backref("versions", lazy='dynamic'))
+    classifiers = association_proxy("_classifiers", "trove",
+                                    creator=Classifier.get_or_create)
+    files = relationship("File", backref="version", )
 
     def __repr__(self):
         ctx = {"name": self.project.name, "version": self.version}
