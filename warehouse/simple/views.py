263553b504fb533a1ec8ce1995369c03e34d787b
Donald Stufft <donald.stufft@gmail.com>
2012-12-16 14:48:29 -0500

    Utilize the yanked attribute to handle deleted versions

--- warehouse/simple/views.py
+++ warehouse/simple/views.py
@@ -6,7 +6,7 @@ import re
 
 import flask
 
-from warehouse.packages.models import Project
+from warehouse.packages.models import Project, Version, File
 
 
 _normalize_regex = re.compile(r"[^A-Za-z0-9.]+")
@@ -35,13 +35,29 @@ def index():
 
 @simple.route("/<project>")
 @simple.route("/<project>/")
+@simple.route("/<project>/<version>")
+@simple.route("/<project>/<version>/")
 @restricted.route("/<project>", defaults={"restricted": True})
 @restricted.route("/<project>/", defaults={"restricted": True})
-def detail(project, restricted=False):
+@restricted.route("/<project>/<version>", defaults={"restricted": True})
+@restricted.route("/<project>/<version>/", defaults={"restricted": True})
+def detail(project, version=None, restricted=False):
     normalized = _normalize_regex.sub("-", project).lower()
     project = Project.query.filter_by(normalized=normalized).first_or_404()
+
+    if version is None:
+        versions = Version.query.filter_by(project=project, yanked=False).all()
+        files = File.query.filter(
+                        File.version.in_(versions), File.yanked == False).all()
+    else:
+        versions = Version.query.filter_by(
+                                    project=project, version=version).all()
+        files = File.query.filter(File.version.in_(versions)).all()
+
     return flask.render_template("detail.html",
                 project=project,
+                versions=versions,
+                files=files,
                 restricted=restricted
             )
 
