2dd7ac08321d986bec3516118fb57f08b5a5fb31
Donald Stufft <donald.stufft@gmail.com>
2012-12-19 22:30:52 -0500

    Add default configuration

---
+++ warehouse/defaults.py
@@ -0,0 +1,29 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+
+# The URI for our PostgreSQL database.
+SQLALCHEMY_DATABASE_URI = "postgres:///warehouse"
+
+# The base domain name for this installation. Used to control linking to
+#      sub-domains.
+SERVER_NAME = "warehouse.local"
+
+# The type of Storage to use. Can be either Filesystem or S3.
+STORAGE = "Filesystem"
+
+# The hash to use in computing filenames.
+#   Allowed values: md5, sha1, sha224, sha256, sha384, sha512, None
+STORAGE_HASH = "md5"
+
+# Base directory for storage when using the Filesystem.
+STORAGE_DIRECTORY = "data"
+
+# The name of the bucket that files will be stored in when using S3.
+# STORAGE_BUCKET = "<storage bucket>"
+
+# The S3 Key used to access S3 when using S3 Storage
+# S3_KEY = "<S3 Key>"
+
+# The S3 Secret used to access S# when using S3 Storage
+# S3_SECRET = "<S3 Secret>"
